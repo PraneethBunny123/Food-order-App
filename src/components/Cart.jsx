06b3c2e98f7444672c72cfadbe39ed8417faf5de
praneeth <gp.gunturu@gmail.com>
2025-04-03 20:42:06 -0500

    handling the close of the modal when the close button inside the Modal is clicked. Also dealing with close modal inside the custom Modal component using the callback function of the useEffect hook, That is when the open value changes the callback function gets executed

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,6 +13,10 @@ export default function Cart() {
         return totalPrice + item.quantity * item.price
     }, 0)
 
+    function handleCloseCart() {
+        userProgressCtx.hideCart()
+    }
+
     return (
         <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
             <h2>Your Cart</h2>
@@ -23,7 +27,7 @@ export default function Cart() {
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
-                <Button textOnly>Close</Button>
+                <Button textOnly onClick={handleCloseCart}>Close</Button>
                 <Button>Go to checkout</Button>
             </p>
         </Modal>
