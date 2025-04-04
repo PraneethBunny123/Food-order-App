947792f48a11af6fc1751dda5023fa13402e51ae
praneeth <gp.gunturu@gmail.com>
2025-04-03 20:24:27 -0500

    Created a UserProgressContextProvided component in store folder. Cause now access the state in different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Header from './components/Header.jsx';
 import Meals from './components/Meals.jsx';
 import { CartContextProvider } from './store/CartContext.jsx';
+import { UserProgressContextProvider } from './store/userProgressContext.jsx';
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header />
-      <Meals />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+      </CartContextProvider>
+    </UserProgressContextProvider>
+
   );
 }
 
