b84cd9926c08cf407c54ce3db01ed8a5dc0e608d
praneeth <gp.gunturu@gmail.com>
2025-04-05 15:54:41 -0500

    Created a Error component that displays an error message when fetching goes wrong, added styles for tht error and center classNames

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,15 +1,21 @@
 import MealItem from "./MealItem"
 import useHttp from "./hooks/useHttp"
+import Error from "./Error"
 
 const requestConfig = {}
 
 export default function Meals() {
-    const {data: loaderMeals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig, [])
+    const {data: loaderMeals, isLoading, error} = useHttp('http://localhost:3000/mealss', requestConfig, [])
 
     if(isLoading) {
-        return <p>Loading ...</p>
+        return <p className="center">Loading ...</p>
     }
 
+    if(error) {
+        return(
+            <Error title='Failed to fetch meals' message={error}/>
+        )
+    }
 
     return (
         <ul id="meals">
